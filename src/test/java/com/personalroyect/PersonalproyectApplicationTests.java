19da0506edfe1188c2d94fd740ceee2f3d8682f9
Sara <sarap120298@gmail.com>
2022-10-18 19:50:00 +0200

    changes

---
+++ src/test/java/com/personalroyect/PersonalproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalroyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalproyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
